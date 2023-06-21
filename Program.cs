b6d1d7f15374dd17dfb96f6cc58f1b9333ed63be
Nguyễn Thị Thu Thuỷ <nguyenthithuthuy0996@gmail.com>
2023-06-21 15:52:12 +0700

    Add Authorzation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using JwtWebAPITutorial.Interfaces.IAuthencation;
 using JwtWebAPITutorial.Providers.Authencation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +12,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition("author", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using Bearer Scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    option.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSetting:TokenSecrectKey").Value)),
+            ValidateAudience =false,
+            ValidateIssuer =false
+        };
+    });
 
 // Register interface and class which we injected
 
@@ -25,6 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
